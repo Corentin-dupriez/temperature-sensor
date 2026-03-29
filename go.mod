0d9fb191c4217234fb4446d7ffb5fa45ff2e04ca
Corentin Dupriez <dupriez.c@icloud.com>
2026-03-29 15:42:31 +0300

    added docker orchestration for redis and pgsql. Added port reading

--- go.mod
+++ go.mod
@@ -4,7 +4,11 @@ go 1.25.0
 
 require (
 	github.com/cespare/xxhash/v2 v2.3.0 // indirect
+	github.com/creack/goselect v0.1.2 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
+	github.com/lib/pq v1.12.0 // indirect
 	github.com/redis/go-redis/v9 v9.18.0 // indirect
+	go.bug.st/serial v1.6.4 // indirect
 	go.uber.org/atomic v1.11.0 // indirect
+	golang.org/x/sys v0.19.0 // indirect
 )
--- main.go
+++ main.go
@@ -2,12 +2,19 @@ package main
 
 import (
 	"context"
+	"errors"
 	"fmt"
 
 	"github.com/redis/go-redis/v9"
+	"go.bug.st/serial"
 )
 
 func main() {
+	port, err := OpenPort()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(port)
 	client := ConnectToDB()
 	ctx := context.Background()
 
@@ -16,17 +23,26 @@ func main() {
 		Values: map[string]interface{}{"temp": "20", "humidity": "40"},
 		ID:     "*",
 	})
-	val, err := client.XRange(ctx, "stream", "-", "+").Result()
-	if err != nil {
-		panic(err)
-	}
+	defer client.Close()
+}
 
-	if len(val) > 0 {
-		for i := 0; i < len(val); i++ {
-			first := val[i].Values
-			fmt.Println(first)
+func OpenPort() (serial.Port, error) {
+	// dev/cu.usbserial-130
+	mode := &serial.Mode{
+		BaudRate: 9600,
+	}
+	ports, err := serial.GetPortsList()
+	if err != nil {
+		return nil, errors.New("not able to read ports")
+	}
+	for _, port := range ports {
+		if port == "dev/cu.usbserial-130" {
+			port, err := serial.Open(port, mode)
+			if err != nil {
+				panic(err)
+			}
+			return port, nil
 		}
 	}
-
-	defer client.Close()
+	return nil, errors.New("arduino is not connected")
 }
