b89e0df9a5adc630f084544c84ced205f581a235
Corentin Dupriez <dupriez.c@icloud.com>
2026-04-02 07:38:57 +0300

    Added readme

---
+++ README.md
@@ -0,0 +1,37 @@
+
+# Temperature sensor dashboard
+
+This repo contains the code for a dashboard application displaying the
+Temperature and humidity in real time.
+
+It is composed of several parts:
+
+```mermaid
+Arduino sensor -> Go Worker
+Go Worker -> Python FastAPI server
+Python FastAPI server -> Vue.js dashboard
+```
+
+## Workflow
+
+### Data generation
+
+The data is generated from an Arduino board, using a DHT11
+Temperature and humidity sensor. The board generates readings every second
+and writes them to the serial port.
+
+### Reading Arduino data
+
+The data generated from Arduino is read by a Go worker, that reads on the
+same serial port, and inserts the data in a Redis stream.
+
+### Serving Redis data
+
+A Python FastAPI server then reads from the Redis stream, and exposes this data
+via a WebSocket
+
+### Displaying the data on a dashboard
+
+The final part is the dashboard, built using Vue.js and chart.js. On mounting,
+the dashboard connects to the FastAPI WebSocket. The displayed data is then
+updated in real-time on the dashboard.
--- server/main.py
+++ server/main.py
@@ -10,25 +10,34 @@ app = FastAPI()
 
 
 @app.websocket("/ws")
-async def websocket_endpoint(websocket: WebSocket):
+async def websocket_endpoint(websocket: WebSocket) -> None:
     await websocket.accept()
     while True:
-        l = rdb.xread(streams={stream_key: 0}, count=1)
-        data = l[0][1][0][1]
-        data = {
-            key.decode("utf-8"): float(value.decode("utf-8"))
-            for key, value in data.items()
-        }
+        entry: list = rdb.xread(streams={stream_key: 0}, count=1)
+        data = parse_data(entry)
         print(data)
         await websocket.send_json({"data": data})
-        while l:
+        while entry:
             await asyncio.sleep(1)
-            last_id = l[0][1][0][0]
-            l = rdb.xread(streams={stream_key: last_id}, count=1)
-            data = l[0][1][0][1]
-            data = {
-                key.decode("utf-8"): float(value.decode("utf-8"))
-                for key, value in data.items()
-            }
-            print(data)
-            await websocket.send_json({"data": data})
+            try:
+                last_id = entry[0][1][0][0]
+                entry = rdb.xread(streams={stream_key: last_id}, count=1)
+                data = parse_data(entry)
+                print(data)
+                await websocket.send_json({"data": data})
+            except IndexError:
+                pass
+
+
+def parse_data(entry: list) -> dict:
+    """Parses a Redis stream row and returns a dictionary containing the temperature and humidity.
+    Args:
+        entry: A Redis stream row containing the row ID and the data associated do the entry.
+
+    Returns:
+        dict: a dictionary containing the temperature and humidity.
+    """
+    data: dict = entry[0][1][0][1]
+    return {
+        key.decode("utf-8"): float(value.decode("utf-8")) for key, value in data.items()
+    }
